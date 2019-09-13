a92de2302cafe99eef270f681cf53c4b63c3d4dc
BigECire <ericsanders5800@gmail.com>
2019-09-12 23:11:36 -0500

    Updates

---
+++ precache-manifest.07b06d910a7126a18748e9a33238a2c1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9528e4164006618fa58038a77f1a13",
+    "url": "/memory/index.html"
+  },
+  {
+    "revision": "2c8ce80319cf9059ddf0",
+    "url": "/memory/static/js/2.b21ec21c.chunk.js"
+  },
+  {
+    "revision": "d247802c537558b894fd",
+    "url": "/memory/static/js/main.ef7d247d.chunk.js"
+  },
+  {
+    "revision": "94f647c09a573e59acd4",
+    "url": "/memory/static/js/runtime~main.91f5a326.js"
+  }
+]);
